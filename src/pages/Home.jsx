e0b37a0fac5485cb7598bc5adacc53fbbc929260
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-07-15 05:30:37 +0000

    Inicio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,60 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className=" container text-center mt-5">
+			<div className="d-flex justify-content-end">
+				<button type="button" className="btn btn-success">Add new contact </button>
+			</div >
+			<div className="d-flex justify-contect-around mt-5 border">
+				<div className="col mt-3 mb-3">
+					<img src="https://picsum.photos/id/237/150/150" className="rounded-circle" alt="..."></img>
+				</div>
+				<div className="col d-flex flex-column justify-content-center align-items-start" >
+						<h3>Mike Anamendolla</h3>
+						<div className="font-serif "><i className="fa-solid fa-location-dot" ></i> 5842 Hillcrest Rd</div>
+						<div className="font-serif "><i className="fa-solid fa-phone-flip"></i> (870) 288-4149</div>
+						<div className="font-serif "><i className="fa-solid fa-envelope "></i> mike_ana@example.com </div>
+				</div>
+				<div className="col d-flex justify-content-center ms-5 mt-4 ">
+					<i className="fa-solid fa-pencil aling-end mx-5"></i>
+                	<i className="fa-solid fa-trash"></i>
+				</div>
+			</div>	
+			<div className="d-flex justify-contect-around border">
+				<div className="col mt-3 mb-3">
+					<img src="https://picsum.photos/id/237/150/150" className="rounded-circle" alt="..."></img>
+				</div>
+				<div className="col d-flex flex-column justify-content-center align-items-start" >
+						<h3>Mike Anamendolla</h3>
+						<div className="font-serif "><i className="fa-solid fa-location-dot" ></i> 5842 Hillcrest Rd</div>
+						<div className="font-serif "><i className="fa-solid fa-phone-flip"></i> (870) 288-4149</div>
+						<div className="font-serif "><i className="fa-solid fa-envelope "></i> mike_ana@example.com </div>
+				</div>
+				<div className="col d-flex justify-content-center ms-5 mt-4 ">
+					<i className="fa-solid fa-pencil aling-end mx-5"></i>
+                	<i className="fa-solid fa-trash"></i>
+				</div>
+			</div>	
+			<div className="d-flex justify-contect-around border">
+				<div className="col mt-3 mb-3">
+					<img src="https://picsum.photos/id/237/150/150" className="rounded-circle" alt="..."></img>
+				</div>
+				<div className="col d-flex flex-column justify-content-center align-items-start" >
+						<h3>Mike Anamendolla</h3>
+						<div className="font-serif "><i className="fa-solid fa-location-dot" ></i> 5842 Hillcrest Rd</div>
+						<div className="font-serif "><i className="fa-solid fa-phone-flip"></i> (870) 288-4149</div>
+						<div className="font-serif "><i className="fa-solid fa-envelope "></i> mike_ana@example.com </div>
+				</div>
+				<div className="col d-flex justify-content-center ms-5 mt-4 ">
+					<i className="fa-solid fa-pencil aling-end mx-5"></i>
+                	<i className="fa-solid fa-trash"></i>
+				</div>
+			</div>	
+			
 		</div>
 	);
 }; 
