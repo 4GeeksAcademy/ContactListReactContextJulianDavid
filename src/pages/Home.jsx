a29987f360090515e0301ebfd80c8c993a5e008c
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-07-17 21:31:04 +0000

    Delete OK :art:

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,43 +4,61 @@ import { useEffect, useState } from "react";
 
 export const Home = () => {
 
-	// const { store, dispatch } = useGlobalReducer()
+  // const { store, dispatch } = useGlobalReducer()
 
-	const [contacts, setContacts ] = useState([]);
+  const [contacts, setContacts] = useState([]);
 
-	const apiUrlGet = "https://playground.4geeks.com/contact/agendas/JulianDavid"
+  const apiUrlGet = "https://playground.4geeks.com/contact/agendas/JulianDavid"
+  const apiUrlDelete = "https://playground.4geeks.com/contact/agendas/JulianDavid/contacts"
+  useEffect(() => {
+    obtenerDatos();
+  }, []);
 
-	useEffect(() =>{
-		obtenerDatos();
-	}, []);
+  const obtenerDatos = async () => {
+    try {
+      const response = await fetch(apiUrlGet);
 
-	const obtenerDatos = async () => {
-		try {
-			const response = await fetch(apiUrlGet);
+      if (!response.ok) {
+        throw new Error(`Error HTTP: ${response.status}`);
+      }
 
-			if (!response.ok) {
-				throw new Error(`Error HTTP: ${response.status}`);
-			}
+      const data = await response.json();
+      setContacts(data.contacts)
+    } catch (error) {
+      console.error('Error al obtener datos:', error);
+    }
+  }
 
-			const data = await response.json();
-			setContacts (data.contacts)
-		} catch (error) {
-			console.error('Error al obtener datos:', error);
-		}
-	}
+  const deleteContact = async (contactId) => {
+    try {
+      await fetch(`${apiUrlDelete}/${contactId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+
+
+      setContacts(contacts.filter(contact => contact.id !== contactId));
+    } catch (error) {
+      console.error("Error al eliminar: ", error);
+      alert("No se pudo eliminar el contacto");
+    }
+  };
 
   return (
     <div className="container text-center mt-5">
-		  <div className="d-flex justify-content-end">
-      		<Link to="/new-contact">
-          		<button type="button" className="btn btn-success mb-3">Add new contact</button>
-     		</Link>
-	  	</div>
+      <div className="d-flex justify-content-end">
+        <Link to="/new-contact">
+          <button type="button" className="btn btn-success mb-3">Add new contact</button>
+        </Link>
+      </div>
 
       {contacts.map((contact) => (
         <div key={contact.id} className="d-flex justify-content-around border">
           <div className="col mt-3 mb-3">
-            <img src="https://picsum.photos/id/237/150/150" className="rounded-circle" alt="contact"/>
+            <img src="https://picsum.photos/id/237/150/150" className="rounded-circle" alt="contact" />
           </div>
           <div className="col d-flex flex-column justify-content-center align-items-start">
             <h3>{contact.name}</h3>
@@ -56,7 +74,9 @@ export const Home = () => {
           </div>
           <div className="col d-flex justify-content-center ms-5 mt-4">
             <i className="fa-solid fa-pencil align-end mx-5"></i>
-            <i className="fa-solid fa-trash" style={{cursor: "pointer"}}></i>
+            <i className="fa-solid fa-trash" style={{ cursor: "pointer" }} onClick={() => {
+              if (window.confirm("¿Estás seguro de eliminar este contacto?")) {
+                deleteContact(contact.id);}}}></i>
           </div>
         </div>
       ))}
